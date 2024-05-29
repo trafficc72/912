76e902f6ec7e5ad06299ead79a05aa679cb7d954
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-29 00:37:31 +0000

    dotnet 8.0.7

--- Formula/d/dotnet.rb
+++ Formula/d/dotnet.rb
@@ -3,8 +3,8 @@ class Dotnet < Formula
   homepage "https://dotnet.microsoft.com/"
   # Source-build tag announced at https://github.com/dotnet/source-build/discussions
   url "https://github.com/dotnet/dotnet.git",
-      tag:      "v8.0.4",
-      revision: "83659133a1aa2b2d94f9c4ecebfa10d960e27706"
+      tag:      "v8.0.7",
+      revision: "8be139ddde52d33e24c7d82f813248ff9fc54b97"
   license "MIT"
 
   bottle do
