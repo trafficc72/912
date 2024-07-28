1115502da803d55dae2e33ec1acc232f637bfb6c
Rui Chen <rui@chenrui.dev>
2024-07-27 20:15:41 -0400

    rpds-py 0.19.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/r/rpds-py.rb
@@ -0,0 +1,77 @@
+class RpdsPy < Formula
+  include Language::Python::Virtualenv
+
+  desc "Python bindings to Rust's persistent data structures"
+  homepage "https://rpds.readthedocs.io/en/latest/"
+  url "https://files.pythonhosted.org/packages/2f/fe/5217efe981c2ae8647b503ba3b8f55efc837df62f63667572b4bb75b30bc/rpds_py-0.19.1.tar.gz"
+  sha256 "31dd5794837f00b46f4096aa8ccaa5972f73a938982e32ed817bb520c465e520"
+  license "MIT"
+
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "rust" => :build
+
+  resource "maturin" do
+    url "https://files.pythonhosted.org/packages/80/da/a4bbd6e97f3645f4ebd725321aa235e22e31037dfd92caf4539f721c0a5a/maturin-1.7.0.tar.gz"
+    sha256 "1ba5277dd7832dc6181d69a005182b97b3520945825058484ffd9296f2efb59c"
+  end
+
+  resource "semantic-version" do
+    url "https://files.pythonhosted.org/packages/7d/31/f2289ce78b9b473d582568c234e104d2a342fd658cc288a7553d83bb8595/semantic_version-2.10.0.tar.gz"
+    sha256 "bdabb6d336998cbb378d4b9db3a4b56a1e3235701dc05ea2690d9a997ed5041c"
+  end
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/32/c0/5b8013b5a812701c72e3b1e2b378edaa6514d06bee6704a5ab0d7fa52931/setuptools-71.1.0.tar.gz"
+    sha256 "032d42ee9fb536e33087fb66cac5f840eb9391ed05637b3f2a76a7c8fb477936"
+  end
+
+  resource "setuptools-rust" do
+    url "https://files.pythonhosted.org/packages/9d/f1/2cb8887cad0726a5e429cc9c58e30767f58d22c34d55b075d2f845d4a2a5/setuptools-rust-1.9.0.tar.gz"
+    sha256 "704df0948f2e4cc60c2596ad6e840ea679f4f43e58ed4ad0c1857807240eab96"
+  end
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.start_with?("python@") }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    ENV.append_path "PATH", buildpath/"bin"
+    pythons.each do |python3|
+      ENV.append_path "PYTHONPATH", buildpath/Language::Python.site_packages(python3)
+
+      deps = %w[setuptools setuptools-rust semantic-version maturin]
+      deps.each do |r|
+        resource(r).stage do
+          system python3, "-m", "pip", "install", *std_pip_args(prefix: buildpath), "."
+        end
+      end
+
+      system python3, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    (testpath/"test.py").write <<~EOS
+      from rpds import HashTrieMap, HashTrieSet, List
+
+      m = HashTrieMap({"foo": "bar", "baz": "quux"})
+      assert m.insert("spam", 37) == HashTrieMap({"foo": "bar", "baz": "quux", "spam": 37})
+      assert m.remove("foo") == HashTrieMap({"baz": "quux"})
+
+      s = HashTrieSet({"foo", "bar", "baz", "quux"})
+      assert s.insert("spam") == HashTrieSet({"foo", "bar", "baz", "quux", "spam"})
+      assert s.remove("foo") == HashTrieSet({"bar", "baz", "quux"})
+
+      L = List([1, 3, 5])
+      assert L.push_front(-1) == List([-1, 1, 3, 5])
+      assert L.rest == List([3, 5])
+    EOS
+
+    pythons.each do |python3|
+      system python3, "test.py"
+    end
+  end
+end
